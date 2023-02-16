b6853eff01d2dd72a8b10119b4c384d4016722bc
Christian Grossmüller <chgad.games@gmail.com>
2023-02-16 15:23:24 +0100

    Implement AppBar with NavigationDrawer.

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,32 @@
 <template>
   <v-app>
-    <NavigationDrawer
-    class="hidden-md-and-up"
-    />
+    <AppBar></AppBar>
     <NavigationBar class="hidden-sm-and-down" />
-
-    <router-view></router-view>
-
+<!--    <router-view></router-view>-->
     <TheFooter />
     <SnackBar />
   </v-app>
 </template>
 
 <script>
-
-import NavigationDrawer from "@/components/AppLayout/NavigationDrawer";
 import NavigationBar from "@/components/AppLayout/NavigationBar";
 import TheFooter from "@/components/AppLayout/TheFooter";
 import SnackBar from "@/components/AppLayout/SnackBar";
+import AppBar from "@/components/AppLayout/AppBar";
 export default {
   name: 'App',
 
   components: {
+    AppBar,
     SnackBar,
     TheFooter,
     NavigationBar,
-    NavigationDrawer
   },
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      drawer: false
+    }
+  },
 }
 </script>
--- src/components/AppLayout/NavigationDrawer.vue
+++ src/components/AppLayout/NavigationDrawer.vue
@@ -1,10 +1,49 @@
 <template>
-  <v-card min-height="50" color="blue" style="width: 100%"></v-card>
+  <v-navigation-drawer temporary location="left" :model-value="modelValue">
+    <v-list>
+      <v-list-item v-for="({title , value}) in items" :value="value" :to="value" :title="title" :key="value" @click="navigate(value)"/>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script>
 export default {
-  name: "NavigationDrawer"
+  props: {
+    modelValue: {
+      default: false,
+      type: Boolean
+    }
+  },
+  emits: ['update:modelValue'],
+  name: "NavigationDrawer",
+  data() {
+    return {}
+  },
+  computed: {
+    items() {
+      return [
+        {
+          title: "One",
+          value: "one",
+        },
+        {
+          title: "Two",
+          value: "two"
+        },
+        {
+          title: "Three",
+          value: "three"
+        }
+      ]
+    }
+  },
+  methods: {
+    // eslint-disable-next-line no-unused-vars
+    navigate(to) {
+      // Navigate logic
+      this.$emit("update:modelValue", false);
+    }
+  }
 }
 </script>
 
