e1b2ec859129a87cef1b8620048658cf38e3b555
Christian Grossmüller <chgad.games@gmail.com>
2023-02-16 17:49:11 +0100

    Change history mode and add global beofre guard for logged in check.

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
-import {createRouter, createWebHashHistory} from "vue-router"
+import {createRouter, createWebHistory} from "vue-router"
 import routes from "./routes"
 
 const router = createRouter({
     // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-    history: createWebHashHistory(),
-    routes, // short for `routes: routes`
+    history: createWebHistory(),
+    routes: routes, // short for `routes: routes`
+})
+
+// eslint-disable-next-line no-unused-vars
+router.beforeEach( async (to, from) => {
+    // TODO: Implement redirect if user is not logged in
+    // return { name: "login"}
 })
 
 export default router;
