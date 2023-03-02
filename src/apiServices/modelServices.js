a0aa590df786ea9c921dabba725d744c6d58a544
Christian Grossmüller <chgad.games@gmail.com>
2023-03-02 14:40:51 +0100

    Implement model modelEndpointServices.

---
+++ src/apiServices/modelServices.js
@@ -0,0 +1,28 @@
+import {
+  crudEndpoint,
+  readOnlyEndpoint
+} from "@/apiServices/baseEndpointService";
+import { Booking } from "@/models/BookingModel";
+import { Building } from "@/models/BuildingModel";
+import { Room } from "@/models/RoomModel";
+import { Workplace } from "@/models/WorkplaceModel";
+
+export const bookingEndpointService = Object.assign(crudEndpoint, {
+  BASE_URL: "/bookings/",
+  MODEL_CLASS: Booking
+});
+
+export const buildingEndpointService = Object.assign(readOnlyEndpoint, {
+  BASE_URL: "/buildings/",
+  MODEL_CLASS: Building
+});
+
+export const roomEndpointService = Object.assign(readOnlyEndpoint, {
+  BASE_URL: "/rooms/",
+  MODEL_CLASS: Room
+});
+
+export const workplaceEndpointService = Object.assign(readOnlyEndpoint, {
+  BASE_URL: "/workplaces/",
+  MODEL_CLASS: Workplace
+});
